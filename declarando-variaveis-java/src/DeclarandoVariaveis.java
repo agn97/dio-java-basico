649b01b11d7c94a820038396d005d2f29ccd1bb3
agn97 <alangomess1997@gmail.com>
2024-04-25 11:45:53 -0300

    declarando variaveis e suas estruturas e tipos

---
+++ declarando-variaveis-java/src/DeclarandoVariaveis.java
@@ -0,0 +1,18 @@
+public class DeclarandoVariaveis {
+    
+public static void main (String [] args) {
+    String meuNome = "Alan";
+
+    double peso = 82.4;
+
+    double altura = 1.82;  
+
+    int anoNascimento = 1997;
+
+    boolean verdadeira = true;
+
+    anoNascimento = 1996; // alteraçao de valor da variavel
+}
+
+
+}
