d4f9b0846609b540b09481f26ed81633ee1b60da
agn97 <alangomess1997@gmail.com>
2024-04-25 09:16:48 -0300

    sintaxe de declaraçao de uma nova classe

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+public class MinhaClasse { // inicio da classe
+    
+    //corpo da classe
+    public static void main (String [] args) {// incio do metodo
+
+        //corpo do metodo
+        System.out.print("Óla Pessoas, Sejam Bem vindos ao Mundo java!!");
+
+    } // fim do metodo
+} // fim da classe
